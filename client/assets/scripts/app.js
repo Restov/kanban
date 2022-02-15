4af1a3bea5d39c7ae05e41b7de3b780d6bb0f15b
Maksim <jeffrimorgan@yandex.ru>
2022-02-15 21:12:37 +0700

    Added columns creation module columns.js

--- client/assets/scripts/app.js
+++ client/assets/scripts/app.js
@@ -1,4 +1,6 @@
+import { createColumn } from "./columns.js";
 import { createjsPanel } from "./new_card.js";
 
 
 $('.add__card').on('click', createjsPanel);
+$('.add__columns').on('click', createColumn);
