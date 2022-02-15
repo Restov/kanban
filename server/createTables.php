ea85719565f7953abb6854e5a691d434a292a831
Roman Bakhmann <romankotovv@gmail.com>
2022-02-15 16:45:03 +0700

    Разбить на файлы

--- server/createTables.php
+++ server/createTables.php
@@ -1,21 +1,5 @@
 <?php
-
-function createQuery($conn, $sql)
-{
-    if (!mysqli_query($conn, $sql)) {
-        echo "Ошибка: " . mysqli_error($conn) . "<br>";
-    }
-}
-
-function createnNewPole($conn, $name, $pos)
-{
-    createQuery($conn, "INSERT poles(name_pole, pos_pole) values($name, $pos)");
-}
-
-function createnNewEvent($conn, $name, $date, $disc, $color, $poleid, $pos)
-{
-    createQuery($conn, "INSERT events (name,date_create,opisanie,color_sob,id_pole,num_pos) values($name,$date,$disc,$color,$poleid,$pos)");
-}
+include "db.php";
 
 
 $hostname = "localhost";
