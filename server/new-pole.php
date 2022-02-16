c797573c584bbd45d8e8ba0c79c1da690f696429
Roman Bakhmann <romankotovv@gmail.com>
2022-02-16 10:32:40 +0700

    Update new-pole.php

--- server/new-pole.php
+++ server/new-pole.php
@@ -9,9 +9,9 @@ $root = $object->root;
 $password = $object->password;
 $db_name = $object->db_name;
 $conn = mysqli_connect($hostname, $root, $password, $db_name);
-if (!$conn) {
+if (!$conn) 
     die("Ошибка: " . mysqli_connect_error());
-}
+
 
 $title = $_POST['title'];
 $id = $_POST['id'];
