cdbd64091cbe4c3bd3ba88215ad55e177346cc2f
Roman Bakhmann <romankotovv@gmail.com>
2022-02-17 20:21:29 +0700

    Update refresh.php

--- server/refresh.php
+++ server/refresh.php
@@ -22,4 +22,17 @@ if (!$conn) {
     die("Ошибка: " . mysqli_connect_error());
 }
 
-echo "213123";
+$SQL = "SELECT * FROM poles";
+$result = mysqli_query($conn,$SQL);
+while ( $db_field = mysqli_fetch_assoc($result) ) {
+    
+    $id = $db_field["id_pole"];
+    $SQL = "SELECT * FROM events WHERE id_pole = '$id'";
+    $resultid = mysqli_query($conn,$SQL);
+    $db_field["cards"] = array();
+    while ( $card = mysqli_fetch_assoc($resultid) ) {
+        array_push($db_field["cards"], $card);
+    }
+    $newArr[] = $db_field;
+}
+echo json_encode($newArr, JSON_UNESCAPED_UNICODE); 
