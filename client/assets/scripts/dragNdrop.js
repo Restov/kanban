7b161ae13659b94520d61434dbd899a4b9e857c3
Maksim <jeffrimorgan@yandex.ru>
2022-02-17 17:34:33 +0700

    Added DragNDrop methods for events in columns

---
+++ client/assets/scripts/dragNdrop.js
@@ -0,0 +1,54 @@
+export function initDraggable() {
+    let nextElement = null;
+    let activeElement = null;
+    let currentList = null;
+
+    $('.column').each(function() {
+        const $events = $(this).find('.column__item');
+
+        $events.each((index, event) => {
+            event.draggable = true
+        });
+
+        $(this).on('dragstart', (event) => {
+            setTimeout(() => event.target.classList.add('selected'), 0);
+            activeElement = event.target;
+            currentList = this;
+        });
+
+        $(this).on('dragend', (event) => {
+            event.target.classList.remove('selected');
+        })
+
+        $(this).on('dragover', (event) => {
+            event.preventDefault();
+
+            const currentElement = event.target;
+
+            const isMoveable = activeElement !== currentElement.parentNode &&
+                    currentElement.parentNode.classList.contains(`column__item`);
+
+            if (!isMoveable) {
+                return;
+            }
+
+            nextElement = (currentElement.parentNode === activeElement.nextElementSibling) ?
+                        currentElement.parentNode.nextElementSibling : currentElement.parentNode;
+
+            $(this).find('.column__content')[0].insertBefore(activeElement, nextElement);
+            nextElement = null
+        })
+
+        $(this).on('drop', () => {
+            if (this !== currentList && !nextElement) {
+                return $(this).find('.column__content')[0].append(activeElement);
+            }
+
+            $(this).find('.column__content')[0].insertBefore(activeElement, nextElement);
+        })
+    });
+};
+
+function sendPositionAfterDrag() {
+
+}
