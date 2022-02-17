c95c775d9e4d2989efcf9950006d3f6269f68ecc
ips912s29 <60769797+ips912s29@users.noreply.github.com>
2022-02-17 23:18:05 +0700

    add import

--- client/assets/scripts/refresh_kanban.js
+++ client/assets/scripts/refresh_kanban.js
@@ -1,14 +1,21 @@
-export function refresh_kanban() {
-    let str;
-    $.ajax({
+export function refreshKanban() {
+    let request = $.ajax({
         type: "GET",
         url: "/server/refresh.php",
         dataType: "text", //expect html to be returned
         success: function(response) {
-            alert(response);
-            str = response;
-            alert(str);
+            console.log(typeof response); // string
+            // console.log(response);
         },
         error: (jqXHR, textStatus, errorThrown) => console.log(textStatus, errorThrown)
+    });
+    request.done(function(msg) {
+        // alert(msg);
+        console.log(JSON.stringify(msg));
+        // console.log(request);
+    });
+
+    request.fail(function(jqXHR, textStatus) {
+        alert("Request failed: " + textStatus);
     });
 }
