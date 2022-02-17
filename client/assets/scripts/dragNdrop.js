3d6f45363ee727d335fbf5bf8f554dd13549f3a0
Maksim <jeffrimorgan@yandex.ru>
2022-02-17 19:42:25 +0700

    Added send position event into column after drag & drop

--- client/assets/scripts/dragNdrop.js
+++ client/assets/scripts/dragNdrop.js
@@ -1,9 +1,9 @@
 export function initDraggable() {
     let nextElement = null;
     let activeElement = null;
-    let currentList = null;
+    let currentColumn = null;
 
-    $('.column').each(function() {
+    $('.column').each(function(index) {
         const $events = $(this).find('.column__item');
 
         $events.each((index, event) => {
@@ -13,7 +13,7 @@ export function initDraggable() {
         $(this).on('dragstart', (event) => {
             setTimeout(() => event.target.classList.add('selected'), 0);
             activeElement = event.target;
-            currentList = this;
+            currentColumn = this;
             nextElement = null
         });
 
@@ -37,16 +37,54 @@ export function initDraggable() {
                         currentElement.parentNode.nextElementSibling : currentElement.parentNode;
 
             $(this).find('.column__content')[0].insertBefore(activeElement, nextElement);
+
+            sendPositionsAfterDrag(activeElement, this, index);
         });
 
         $(this).on('drop', () => {
-            if (this !== currentList && !nextElement) {
+            if (this !== currentColumn && !nextElement) {
                 $(this).find('.column__content')[0].append(activeElement);
+
+                sendPositionsAfterDrag(activeElement, this, index);
             }
         });
     });
 };
 
-function sendPositionAfterDrag() {
+function getPositionsIntoColumn(currentColumn, idColumn) {
+    let positions = '';
 
+    const $events = $(currentColumn).find('.column__item');
+    $events.each((index, event) => {
+        let idEvent = event.getAttribute('data-id-event');
+
+        positions += `${idColumn}:${idEvent} `;
+    })
+
+    return positions.trim();
+}
+
+function initRecvDataAfterDrag(currentColumn, idEvent, idColumn) {
+    let positions = getPositionsIntoColumn(currentColumn, idColumn);
+
+    const data = {
+        idEvent,
+        idColumn,
+        positions
+    };
+
+    return data;
+}
+
+function sendPositionsAfterDrag(activeElement, currentColumn, index) {
+    let idEvent = activeElement.getAttribute('data-id-event');
+    let data = initRecvDataAfterDrag(currentColumn, idEvent, index);
+
+    $.ajax({
+        type: 'POST',
+        url: '/server/update-pos.php',
+        data,
+        success: () => console.log('Positions have been updated.'),
+        error: (jqXHR, textStatus, errorThrown) => console.log(textStatus, errorThrown)
+    })
 }
