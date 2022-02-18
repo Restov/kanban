415f09e8a83e271df0168349a3890f34cc1a8ec5
Roman Bakhmann <romankotovv@gmail.com>
2022-02-18 14:16:29 +0700

    Update update-pos.php

--- server/update-pos.php
+++ server/update-pos.php
@@ -16,3 +16,28 @@ if (!$conn) {
 }
 
 var_dump($_POST);
+$id_ev = $_POST["idEvent"];
+$sql = "SELECT * FROM events WHERE id = '$id_ev'";
+$result = mysqli_query($conn,$sql);
+$db_field = mysqli_fetch_assoc($result);
+var_dump($db_field);
+$lastpolid = $db_field["id_pole"];
+$lastpolpos= $db_field["num_pos"];
+
+
+$newcol = $_POST["idColumn"];
+
+$positions = $_POST["positions"];
+$positions = explode(" ",$positions);
+$newpos = array();
+foreach($positions as $pos){
+    $pos = substr($pos, 2);
+    array_push($newpos,$pos); 
+}
+
+var_dump($newpos);
+for($i = 0; $i < count($newpos); ++$i)
+{
+    $temp = $newpos[$i];
+    createQuery($conn,"UPDATE events SET num_pos = '$i' WHERE (id_pole = '$newcol' AND id = '$temp')");
+}
