93ee0b1a0a1d01af62cd17861280c2c03524f7f8
Maksim <jeffrimorgan@yandex.ru>
2022-02-20 15:59:10 +0700

    Applyed codestyle and ES6 in new_card.js

--- client/assets/scripts/new_card.js
+++ client/assets/scripts/new_card.js
@@ -9,8 +9,8 @@ export function createjsPanel() {
         content: '<input type="text" id="name" /> <input type="date" id="date" /><input type="color" id="color" /><input type="text" id="disc" /><p><button class="modal__btn">Send</button></p> '
     });
 
-    $('.modal__btn').on('click', send)
-    
+    $('.modal__btn').on('click', send);
+
 }
 
 function send() { // убери это потом пж)
@@ -18,35 +18,35 @@ function send() { // убери это потом пж)
     var date = $('#date').val();
     var color = $('#color').val();
     var disc = $('#disc').val();
+
     let $card = $(
-        '<div class="column__item" draggable="true">  \
-        <div class="item__inner">\
-            <div class="item__priority" style="background-color:'+ color + ';" >\
-            </div>\
-            <div class="item__head">\
-                <span class="item__title">' + name + '</span>\
-                <span class="item__date">' + date + '</span>\
-            </div>\
-            <div class="item__content">\
-                <span class="item__textContent">'+ disc +'</span>\
-            </div>\
-        </div>\
-    </div>')
-   
+        `<div class="column__item">
+            <div class="item__inner">
+                <div class="item__priority" style="background-color:${color}"></div>
+                <div class="item__head">
+                    <span class="item__title">${name}</span>
+                    <span class="item__date">${date}</span>
+                </div>
+                <div class="item__content">
+                    <span class="item__textContent">${disc}</span>
+                </div>
+            </div>
+        </div>`);
+
     $.ajax({
         type: "POST",
         url: "/server/new-card.php",
-        data: { name: name, date: date, color: color, disc: disc },
+        data: { name, date, color, disc },
         success: (response) => {
-            
+
             if(response.match(/^[0-9]+$/) != null) {
-                $(".column__content:first").prepend($card)
-                $(".column__item:first").attr("data-id-event",response)
+                $(".column__content:first").prepend($card);
+                $(".column__item:first").attr("data-id-event",response);
+            } else {
+                alert(response);
             }
-            else
-            alert(response)
         } ,
         error: (jqXHR, textStatus, errorThrown) => console.log(textStatus, errorThrown)
     });
-    
+
 }
