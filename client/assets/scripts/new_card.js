51e78c3b69040a959321261747c32a2784a63118
Maksim <jeffrimorgan@yandex.ru>
2022-02-23 02:23:47 +0700

    Added render columns and cards

--- client/assets/scripts/new_card.js
+++ client/assets/scripts/new_card.js
@@ -1,3 +1,5 @@
+import { refreshKanban } from "./refresh_kanban.js";
+
 export function createjsPanel() {
     jsPanel.create({
         headerTitle: "Новая карточка",
@@ -12,38 +14,19 @@ export function createjsPanel() {
     $('.modal__btn').on('click', send);
 }
 
-function send() { // убери это потом пж)
+function send() {
     let name = $('#name').val();
     let date = $('#date').val();
     let color = $('#color').val();
     let disc = $('#disc').val();
 
-    let $card = $(
-        `<div class="column__item">
-            <div class="item__inner">
-                <div class="item__priority" style="background-color:${color}"></div>
-                <div class="item__head">
-                    <span class="item__title">${name}</span>
-                    <span class="item__date">${date}</span>
-                </div>
-                <div class="item__content">
-                    <span class="item__textContent">${disc}</span>
-                </div>
-            </div>
-        </div>`);
-
     $.ajax({
         type: "POST",
         url: "/server/new-card.php",
         data: { name, date, color, disc },
-        success: (response) => {
-            if(response.match(/^[0-9]+$/) != null) {
-                $(".column__content:first").prepend($card);
-                $(".column__item:first").attr("data-id-event",response);
-            } else {
-                alert(response);
-            }
-        } ,
+        success: () => {
+            refreshKanban();
+        },
         error: (jqXHR, textStatus, errorThrown) => console.log(textStatus, errorThrown)
     });
 }
