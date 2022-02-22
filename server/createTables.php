e58b44d1f033493211d870248179f736642b482d
Roman Bakhmann <romankotovv@gmail.com>
2022-02-23 03:50:36 +0700

    Create new pole if its not exist

--- server/createTables.php
+++ server/createTables.php
@@ -1,6 +1,13 @@
 <?php
 include "db.php";
 
+function getPolesCount($conn)
+{
+    $res = $conn->query("SELECT count(*) FROM poles");
+    $row = $res->fetch_row();
+    return $row[0];
+}
+
 $ourData = file_get_contents("../client/assets/data.json");
 
 $object = json_decode($ourData);
@@ -22,7 +29,11 @@ if (!$conn) {
 }
 
 createQuery($conn, "CREATE TABLE IF NOT EXISTS poles (id_pole INTEGER AUTO_INCREMENT PRIMARY KEY, name_pole VARCHAR(30), pos_pole INTEGER);");
-createnNewPole($conn, "Пустое поле", 0);
+
+
+if (getPolesCount($conn) == 0)
+    createnNewPole($conn, "Пустое поле", 0);
+
 createQuery($conn, "CREATE TABLE IF NOT EXISTS events (id INTEGER AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30),
 date_create DATE, opisanie VARCHAR(100), color_sob VARCHAR(7), id_pole INTEGER, num_pos INTEGER);");
 mysqli_close($conn);
