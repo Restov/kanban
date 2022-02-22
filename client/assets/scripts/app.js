e3530fb731ddf66a3fe4bd65b27c759ed6ead9f3
Maksim <jeffrimorgan@yandex.ru>
2022-02-23 03:45:03 +0700

    Create tables after load page

--- client/assets/scripts/app.js
+++ client/assets/scripts/app.js
@@ -1,5 +1,12 @@
 import { createColumn } from "./columns.js";
 import { refreshKanban } from "./refresh_kanban.js";
 
-$(document).ready(refreshKanban);
+$(document).ready(() => {
+    $.ajax({
+        type: 'GET',
+        url: '/server/createTables.php'
+    });
+
+    refreshKanban();
+});
 $('.add__columns').on('click', createColumn);
